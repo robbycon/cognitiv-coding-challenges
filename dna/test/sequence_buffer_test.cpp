28cee295caadcd8b1557394bc81f6b6d336d51b6
Robby Connor <robbycon@gmail.com>
2023-03-07 15:33:10 -0800

    Added test cases for seqence comparer

--- dna/test/sequence_buffer_test.cpp
+++ dna/test/sequence_buffer_test.cpp
@@ -74,3 +74,73 @@ TEST_CASE("Sequence buffer compare 1 mismatch at beginning", "[stream]")
 	REQUIRE(mismatched_intervals[0].first == 0);
 	REQUIRE(mismatched_intervals[0].second == 0);
 }
+
+TEST_CASE("Sequence buffer compare 1 mismatch with length 2 in middle", "[stream]")
+{
+	const std::array<std::byte, 2> data1 = {
+			dna::pack(dna::T, dna::A, dna::C, dna::T),
+			dna::pack(dna::A, dna::A, dna::G, dna::C),
+	}, data2 = {
+			dna::pack(dna::T, dna::A, dna::G, dna::G),
+			dna::pack(dna::A, dna::A, dna::G, dna::C),
+	};
+	dna::sequence_buffer buf1(data1), buf2(data2);
+	
+	const auto mismatched_intervals = compare(buf1, buf2);
+	REQUIRE(mismatched_intervals.size() == 1);
+	REQUIRE(mismatched_intervals[0].first == 2);
+	REQUIRE(mismatched_intervals[0].second == 3);
+}
+
+TEST_CASE("Sequence buffer compare 1 mismatch at end", "[stream]")
+{
+	const std::array<std::byte, 2> data1 = {
+			dna::pack(dna::T, dna::A, dna::C, dna::T),
+			dna::pack(dna::A, dna::A, dna::G, dna::C),
+	}, data2 = {
+			dna::pack(dna::T, dna::A, dna::C, dna::T),
+			dna::pack(dna::A, dna::A, dna::G, dna::G),
+	};
+	dna::sequence_buffer buf1(data1), buf2(data2);
+	
+	const auto mismatched_intervals = compare(buf1, buf2);
+	REQUIRE(mismatched_intervals.size() == 1);
+	REQUIRE(mismatched_intervals[0].first == 7);
+	REQUIRE(mismatched_intervals[0].second == 7);
+}
+
+TEST_CASE("Sequence buffer compare all equal but different lengths", "[stream]")
+{
+	const std::vector<std::byte> data1 = {
+			dna::pack(dna::T, dna::A, dna::C, dna::T),
+			dna::pack(dna::A, dna::A, dna::G, dna::C),
+	}, data2 = {
+			dna::pack(dna::T, dna::A, dna::C, dna::T),
+			dna::pack(dna::A, dna::A, dna::G, dna::C),
+			dna::pack(dna::A, dna::A, dna::A, dna::A),
+	};
+	dna::sequence_buffer buf1(data1), buf2(data2);
+	
+	const auto mismatched_intervals = compare(buf1, buf2);
+	REQUIRE(mismatched_intervals.size() == 1);
+	REQUIRE(mismatched_intervals[0].first == 8);
+	REQUIRE(mismatched_intervals[0].second == 11);
+}
+
+TEST_CASE("Sequence buffer compare 1 mismatch at end and different lengths", "[stream]")
+{
+	const std::vector<std::byte> data1 = {
+			dna::pack(dna::T, dna::A, dna::C, dna::T),
+			dna::pack(dna::A, dna::A, dna::G, dna::C),
+	}, data2 = {
+			dna::pack(dna::T, dna::A, dna::C, dna::T),
+			dna::pack(dna::A, dna::A, dna::G, dna::G),
+			dna::pack(dna::A, dna::A, dna::G, dna::G),
+	};
+	dna::sequence_buffer buf1(data1), buf2(data2);
+	
+	const auto mismatched_intervals = compare(buf1, buf2);
+	REQUIRE(mismatched_intervals.size() == 1);
+	REQUIRE(mismatched_intervals[0].first == 7);
+	REQUIRE(mismatched_intervals[0].second == 11);
+}
